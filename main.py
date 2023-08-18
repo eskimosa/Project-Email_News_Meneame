83b7eae7fa2ff8164f4f74259563cc6ad38a1cde
eskimosa <esskimosa@gmail.com>
2023-08-18 09:52:36 +0200

    Modified path to file with env variables using __file__

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import dotenv
 import scrape_web
 import send_email
+import os
 
 
 def start():
-    dotenv.load_dotenv(dotenv_path='/Users/jenya/Desktop/becoming_full_stack/python/my_projects/scrape_and_send_news_meneame/.env')
+    dotenv.load_dotenv(dotenv_path=os.path.join(os.path.abspath(__file__), '.env'))
     return send_email.email(scrape_web.scrape_news())
 
 
 if __name__ == '__main__':
     start()
-
